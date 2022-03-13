399e2336f55454caaf247d5105d6e5c00fb88567
369 <rob@jackbadger.co.uk>
2022-03-13 12:08:57 +0000

    Add project files.

---
+++ QR/IQRGen.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="IQRGen.cs" company="TheFriendyCodlingCompany">
+// Copyright (c) TheFriendyCodlingCompany. All rights reserved.
+// </copyright>
+
+namespace QÖmürR.QR
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// The q r generator.
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("6665C9A9-EF6B-408E-8E6D-F9DEEE381C09")]
+    [TypeLibImportClass(typeof(QRGen))]
+    public interface IQRGen
+    {
+        /// <summary>
+        /// Inits the qr generator.
+        /// </summary>
+        /// <returns>A bool.</returns>
+        bool Init();
+
+        /// <summary>
+        /// Creates the q r.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns>A string containing the temp file path to the image.</returns>
+        string CreateQR(string text);
+    }
+}
---
+++ QR/QRGen.cs
@@ -0,0 +1,68 @@
+﻿// <copyright file="QRGen.cs" company="TheFriendyCodlingCompany">
+// Copyright (c) TheFriendyCodlingCompany. All rights reserved.
+// </copyright>
+
+namespace QÖmürR.QR
+{
+    using System;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Runtime.InteropServices;
+    using QRCoder;
+    using QRCoder.Exceptions;
+    using QRCoder.Extensions;
+
+    /// <summary>
+    /// The q r gen.
+    /// </summary>
+    [ComVisible(true)]
+    [Guid("4ADC7747-B8C1-41DE-AEE3-4E245FA4178E")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class QRGen : IQRGen
+    {
+        private readonly string tempPath;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="QRGen"/> class.
+        /// </summary>
+        public QRGen()
+        {
+            this.tempPath = Path.GetTempPath();
+        }
+
+        /// <inheritdoc/>
+        public string CreateQR(string text)
+        {
+            try
+            {
+                string path;
+
+                using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+                {
+                    QRCodeData qrCodeData = qrGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+                    QRCode qrCode = new QRCode(qrCodeData);
+                    Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+                    Path.GetRandomFileName();
+
+                    path = this.tempPath + Path.GetRandomFileName() + ".png";
+
+                    qrCodeImage.Save(path, ImageFormat.Png);
+                }
+
+                return path;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        /// <inheritdoc/>
+        public bool Init()
+        {
+            return true;
+        }
+    }
+}
